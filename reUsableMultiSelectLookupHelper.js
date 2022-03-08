717cc5029bde06389a372b64bfef8ecfcd22a67f
Ashish Kumar <52946953+ashish2889@users.noreply.github.com>
2022-03-08 22:50:33 +0530

    Create reUsableMultiSelectLookupHelper.js

---
+++ reUsableMultiSelectLookupHelper.js
@@ -0,0 +1,30 @@
+({
+    searchHelper : function(component,event,getInputkeyWord) {
+        // call the apex class method 
+        var action = component.get("c.fetchLookUpValues");
+        // set param to method  
+        action.setParams({
+            'searchKeyWord': getInputkeyWord,
+            'ObjectName' : component.get("v.objectAPIName"),
+            'ExcludeitemsList' : component.get("v.lstSelectedRecords")
+        });
+        // set a callBack    
+        action.setCallback(this, function(response) {
+            $A.util.removeClass(component.find("mySpinner"), "slds-show");
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                var storeResponse = response.getReturnValue();
+                // if storeResponse size is equal 0 ,display No Records Found... message on screen.                }
+                if (storeResponse.length == 0) {
+                    component.set("v.Message", 'No Records Found...');
+                } else {
+                    component.set("v.Message", '');
+                    // set searchResult list with return value from server.
+                }
+                component.set("v.listOfSearchRecords", storeResponse); 
+            }
+        });
+        // enqueue the Action  
+        $A.enqueueAction(action);
+    },
+})
